57680e55ecf425b7f6361f17cecd0561f0085286
mizus <xxmizusxx@gmail.com>
2015-12-16 11:54:14 +0100

    Mutex und GameService_1

--- src/main/java/restopoly/services/GameService.java
+++ src/main/java/restopoly/services/GameService.java
@@ -183,6 +183,7 @@ public class GameService implements Ports{
 //      responses: 200 - already holding the mutex,
 //      201 - aquired the mutex,
 //      409 - already aquired by an other player
+
         put("/games/:gameid/players/:playerid/turn", (req, res) -> {
             res.status(409);
             String gameid = req.params(":gameid");
